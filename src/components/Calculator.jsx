62b1809acd446f5a3d7f648d6c3d04689fd1e824
Rahul Kumar <rahulrai2822march@gmail.com>
2025-06-26 16:19:04 +0530

    ui updates

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,8 +3,22 @@ import styles from "./Calculator.module.css";
 
 const Calculator = () => {
   return (
-    <div className={`container  m-auto border mt-5 ${styles.main_container}`}>
-      Calculator
+    <div className={`container  m-auto  border mt-5 ${styles.main_container}`}>
+      <input
+        id="input-box"
+        type="number"
+        className="form-control mt-2"
+        placeholder="Type..."
+      />
+      <div className="row mt-3">
+        {Array.from({ length: 10 }, (_, index) => (
+          <div className="col-4">
+            <button type="button" className="btn btn-primary mt-1 w-100">
+              {index}
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
