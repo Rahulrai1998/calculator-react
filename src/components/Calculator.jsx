39478120226523075e9f173d4aba883eac98f42d
Rahul Kumar <rahulrai2822march@gmail.com>
2025-06-26 15:39:41 +0530

    calculator initiated

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Calculator.module.css";
+
+const Calculator = () => {
+  return (
+    <div className={`container  m-auto border mt-5 ${styles.main_container}`}>
+      Calculator
+    </div>
+  );
+};
+
+export default Calculator;
